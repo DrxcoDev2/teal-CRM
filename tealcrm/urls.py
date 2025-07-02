601648d15e700d633b4f134888bfd0bede5f94a1
DrxcoDev2 <drxcofn2023@gmail.com>
2025-07-02 18:54:04 +0200

    add signup

--- tealcrm/urls.py
+++ tealcrm/urls.py
@@ -2,9 +2,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from core.views import index
+from core.views import index, about
+from userprofile.views import singup
 
 urlpatterns = [
     path('', index, name='index'),
+    path('about/', index, name='about'),
+    path('sign-up/', index, name='signup'),
     path('admin/', admin.site.urls),
 ]
